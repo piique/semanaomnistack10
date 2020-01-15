bb5143feb99ad4422b32b5a7e385007eb1615aa9
Pedro Vilaca <phvilaca@yahoo.com.br>
2020-01-14 23:26:05 -0300

    Alterada formatação e adicionando novas funções

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@lowstore-7lyq0.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect(
+  'mongodb+srv://omnistack:omnistack@lowstore-7lyq0.mongodb.net/week10?retryWrites=true&w=majority',
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-});
+  }
+);
 
 app.use(express.json());
 app.use(routes);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
-const DevController = require('./controllers/DevController')
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 const routes = Router();
-
 
 // Desenvolvedores
 routes.post('/devs', DevController.store);
-routes.delete('/dev', DevController.delete);
-routes.get('/devs', DevController.devs);
+routes.delete('/dev', DevController.destroy);
+routes.get('/devs', DevController.index);
 
+routes.post('/search', SearchController.index);
 
 module.exports = routes;
