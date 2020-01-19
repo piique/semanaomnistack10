d3ae4dcbb5600ecf5b991a8086341fc44f0f0e5f
Pedro Vilaca <phvilaca@yahoo.com.br>
2020-01-19 12:07:50 -0300

    Criada aplicação frontend web de forma funcional - cadastrando usuários

--- web/src/App.js
+++ web/src/App.js
@@ -1,58 +1,57 @@
-import React, { useState } from 'react';
-import './global.css';
-import './App.css';
-import './Sidebar.css'
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+
+import "./Main.css";
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
 // Componente: bloco isoladod e HTML, CSS e JS o qual não interfere no restante da aplicação
 // Propriedade: (title="Dashboard") informações que um compoente PAI passa para um componente filho. Pode ser passado String, função, variavel
 // Estado: informação mantida pelo componente. (Lembrar: imutabilidade)
 
 // Componente Header
-import Header from './Header';
+// import Header from "./Header";
 
 function App() {
-  
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+
+    //não pode ser dado um array.push
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-            
-          <div class="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-
-          <div className="input-group">
-            <div class="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required/>
-            </div>
-            <div class="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/>
-            </div>
-          </div>
-          
-          <button type="submit">Salvar</button>
-
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
+
       <main>
-
+        <ul>
+          {devs.map(dev => {
+            return <DevItem key={dev._id} dev={dev} />;
+          })}
+        </ul>
       </main>
-
     </div>
   );
 }
-
-
-
-
 
 /*function App() {
   const [counter, setCounter] = useState(0);
---
+++ web/src/components/DevForm/index.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+
+import "./styles.css";
+
+function DevForm({ handleAddDev }) {
+  const [github_username, setGithubUsername] = useState("");
+  const [techs, setTechs] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+        // document.getElementById('latitude').value = latitude // html normal
+      },
+      err => {
+        console.log(err);
+      },
+      {
+        timeout: 30000
+      }
+    );
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault(); // evitar comportamento padrão do form de enviar o usuário para outra tela
+
+    await handleAddDev({
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setGithubUsername("");
+    setTechs("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="input-block">
+        <label htmlFor="github_username">Usuário do Github</label>
+        <input
+          name="github_username"
+          type="String"
+          id="github_username"
+          required
+          value={github_username}
+          onChange={e => setGithubUsername(e.target.value)}
+        />
+      </div>
+
+      <div className="input-block">
+        <label htmlFor="techs">Tecnologias</label>
+        <input
+          name="techs"
+          id="techs"
+          required
+          value={techs}
+          onChange={e => setTechs(e.target.value)}
+        />
+      </div>
+
+      <div className="input-group">
+        <div className="input-block">
+          <label htmlFor="latitude">Latitude</label>
+          <input
+            type="number"
+            name="latitude"
+            id="latitude"
+            required
+            value={latitude}
+            onChange={e => setLatitude(e.target.value)}
+          />
+        </div>
+        <div className="input-block">
+          <label htmlFor="longitude">Longitude</label>
+          <input
+            type="number"
+            name="longitude"
+            id="longitude"
+            required
+            value={longitude}
+            onChange={e => setLongitude(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <button type="submit">Salvar</button>
+    </form>
+  );
+}
+
+export default DevForm;
