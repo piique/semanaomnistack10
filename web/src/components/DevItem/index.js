322c5240eb18851968e00ae51a9585e7f6716cc5
Pedro Valverde <pedro.valverde@teknisa.com>
2020-01-20 17:57:09 -0300

    Adicionado botao de excluir. Falta criar acao para ele

--- web/src/components/DevItem/index.js
+++ web/src/components/DevItem/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 import "./styles.css";
+import DeleteIcon from '@material-ui/icons/Delete';
+
 
 function DevItem({ dev }) {
   return (
     <li key={dev._id} className="dev-item">
+      <DeleteIcon className="delete-user-icon"/>
       <header>
         <img src={dev.avatar_url} alt={dev.name} />
         <div className="user-info">
